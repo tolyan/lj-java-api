456e93e611a2c98dfbed0a94a2b5ce24e5f8c06e
Anatoly <arybalchenko@enkata.com>
2012-10-03 15:15:06 +0400

    initial commit

---
+++ src/main/java/ru/anglerhood/lj/App.java
@@ -0,0 +1,13 @@
+package ru.anglerhood.lj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
